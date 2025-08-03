2431cf854d2dbe4b2622a6404b2f235a5008669c
stijnv18 <verheyden.stijn2@gmail.com>
2025-08-03 14:23:24 +0200

    Add incidents app with views, templates, and URL routing

--- incidents/views.py
+++ incidents/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, redirect
+from .models import Incident
+from django import forms
+
+class IncidentForm(forms.ModelForm):
+    class Meta:
+        model = Incident
+        fields = ['friend_name', 'description', 'date', 'clumsy_level']
+
+def home(request):
+    incidents = Incident.objects.order_by('-date')[:10]
+    return render(request, 'incidents/home.html', {'incidents': incidents})
+
+def add_incident(request):
+    if request.method == 'POST':
+        form = IncidentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = IncidentForm()
+    return render(request, 'incidents/add_incident.html', {'form': form})
